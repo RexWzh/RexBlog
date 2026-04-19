9175a76b239279f98109a5e51fc54b14c895637d
rexwzh <1073853456@qq.com>
2026-04-20 06:33:53 +0800

    style: tweak terminal code block spacing and add expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
 
 export default defineConfig({
   site: 'http://www.wzhecnu.cn',
   output: 'static',
   outDir: './public',
   publicDir: './static',
+  integrations: [
+    expressiveCode({
+      themes: ['github-dark'],
+      styleOverrides: {
+        borderRadius: '0px',
+        borderWidth: '2px',
+        borderColor: 'var(--text)',
+        frames: {
+          shadowColor: 'transparent',
+          frameBoxShadowCssValue: '4px 4px 0 var(--text)',
+        }
+      }
+    })
+  ],
   markdown: {
     shikiConfig: {
       theme: 'github-light',
